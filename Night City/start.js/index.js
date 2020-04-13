8fad8d991aeff4fa25f8010ea3d0c6878da04f96
NoHaxJustUrNoob <62440713+NoHaxJustUrNoob@users.noreply.github.com>
2020-04-13 12:01:53 +0200

    Add files via upload

---
+++ Night City/start.js/index.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js')
+const Discord = new Discord.Client()
+
+let prefix = "*"
+
+client.login('Njk4ODU5MzQyMzg4MDAyOTI2.XpL9oA.Bd6rzeX402P1eJVQMewi0qlDu6E')
+client.on('ready', function(){
+
+    console.log('Hey, je suis le bot personnalisé de Night City ! Je suis connecté !')
+    client.user.setActivity('Hey, je suis le bot personnalisé de Night City, je suis en codage !')
+
+})
